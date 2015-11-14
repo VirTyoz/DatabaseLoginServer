f183c41214cd6453744785c31a0c7d9c5a69dc92
Nyanta <petewindows2011@yandex.ru>
2015-11-14 18:53:16 +0300

    Write Server List Method

--- src/database/service/LoginServiceHandler.java
+++ src/database/service/LoginServiceHandler.java
@@ -9,25 +9,24 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.thrift.TException;
 
-import database.DatabaseServer;
-
 public class LoginServiceHandler implements LoginService.Iface{
 	private Connection loginServerDB;
 	private Statement loginServerStmt;
 	private ResultSet loginServerRS;
+	public volatile ServerList serverList;
 	
 	private static final Logger logger = LogManager.getLogger(LoginServiceHandler.class);
 	public LoginServiceHandler(Connection loginServerDB) throws SQLException{
 		this.loginServerDB=loginServerDB;
 		loginServerStmt = loginServerDB.createStatement();
+		serverList =  new ServerList();
 	}
 
-	@Override
-	public int authorization(String name, String password) throws TException {
+	public int authorization(String login, String password) throws TException {
 		int status = 0;
 		try {
 			
-			loginServerRS = loginServerStmt.executeQuery("SELECT * FROM accounts WHERE login = '"+name+"'AND password = '"+password+"'");
+			loginServerRS = loginServerStmt.executeQuery("SELECT * FROM accounts WHERE login = '"+login+"'AND password = '"+password+"'");
 			
 			if(loginServerRS.next()){
 				
@@ -47,9 +46,24 @@ public class LoginServiceHandler implements LoginService.Iface{
 	}
 
 	@Override
-	public String serverList() throws TException {
-		// TODO Auto-generated method stub
-		return null;
+	public ServerList serverList() throws TException {
+		try {
+			loginServerRS = loginServerStmt.executeQuery("SELECT * FROM gameservers");
+			
+			while(loginServerRS.next()){
+				serverList.id = 8;
+				serverList.serverIp = loginServerRS.getString("host");
+				serverList.serverPort = loginServerRS.getShort("port");
+				serverList.serverAgeLimit = loginServerRS.getShort("age_limit");
+				serverList.serverType = loginServerRS.getShort("type");
+				serverList.serverOnlineLimit = loginServerRS.getShort("online_limit");
+				
+				logger.info("ServerList Ok");
+			}
+		} catch (Exception e) {
+			logger.catching(e);
+		}
+		return serverList;
 	}
 
 }
